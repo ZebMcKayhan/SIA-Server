b4c93e290d4084858ed070ffe27b106f8a709314
ZebMcKayhan <92390878+ZebMcKayhan@users.noreply.github.com>
2026-03-09 19:18:36 +0100

    Prepare Release 1.9.0

--- sia-server.py
+++ sia-server.py
@@ -7,7 +7,7 @@ Honeywell Galaxy Flex alarm systems. It sends notifications via ntfy.sh.
 This server is configured via 'sia-server.conf' and 'configuration.py'.
 """
 # --- Application Version ---
-__version__ = "1.9.0-beta"
+__version__ = "1.9.0"
 
 import asyncio
 import logging
