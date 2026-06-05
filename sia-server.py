7a6c3baa6244177f5768789201c42da4ca215d9d
ZebMcKayhan <92390878+ZebMcKayhan@users.noreply.github.com>
2026-06-05 09:25:18 +0200

    Bump version from 2.3.0-beta2 to 2.3.0-beta3

--- sia-server.py
+++ sia-server.py
@@ -8,7 +8,7 @@ Honeywell Galaxy Flex alarm systems. It sends notifications via ntfy.sh.
 This server is configured via 'sia-server.conf' and 'configuration.py'.
 """
 # --- Application Version ---
-__version__ = "2.3.0-beta2"
+__version__ = "2.3.0-beta3"
 
 import argparse
 import asyncio
