aff93a75b7990937c1aa33fd4c015e2b711bc83a
ZebMcKayhan <92390878+ZebMcKayhan@users.noreply.github.com>
2026-08-27 21:10:01 +0200

    2.7.0-beta_5 - custom IP-Check notifications
    2.7.0-beta_5 - custom IP-Check notifications
    =============================
    - 4 notification events added:
    MONITORING_STARTED, CONNECTION_RESTORED, INTERVAL_CHANGED, WATCHDOG_TIMEOUT
    - Custom time formats: Time_source{%YYYY, %YY, %MM, %DD, %hh, %mm, %ss}
    - Custom duration formats: duration_source{%DD, %hh, %mm, %ss} which always folds into lower significant values
    - ascii fields: %account, %site_name, %current_state, %new_state
    - Time fields: %last_panel_time, %last_seen, %new_panel_time
    - Server time: %YYYY, %YY, %MM, %DD, %hh, %mm, %ss, %time
    - Duration fields: %last_interval, %new_interval, %last_threshold, %new_threshold, %elapsed
    - last_* represents last (stored) known data, if any
    - new_* represents incoming data on heartbeat events.
    
    - last_* is not available on `MONITORING_STARTED` events as there have not been a ping pefore.
    - new_* is not available on `WATCHDOG_TIMEOUT`events as this was a timeout and no new ping received.

--- sia-server.py
+++ sia-server.py
@@ -8,7 +8,7 @@ Honeywell Galaxy Flex alarm systems. It sends notifications via ntfy.sh.
 This server is configured via 'sia-server.conf' and 'configuration.py'.
 """
 # --- Application Version ---
-__version__ = "2.7.0-beta_4"  #
+__version__ = "2.7.0-beta_5"  #
 
 import argparse
 import asyncio
