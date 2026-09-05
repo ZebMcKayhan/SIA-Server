aec4e329e0f5072d5330d36ddf16b3b3b67d9bd0
ZebMcKayhan <92390878+ZebMcKayhan@users.noreply.github.com>
2026-09-05 22:33:36 +0200

    Update application version to 2.7.0-RC1

--- sia-server.py
+++ sia-server.py
@@ -8,7 +8,7 @@ Honeywell Galaxy Flex alarm systems. It sends notifications via ntfy.sh.
 This server is configured via 'sia-server.conf' and 'configuration.py'.
 """
 # --- Application Version ---
-__version__ = "2.7.0-beta_6"  #
+__version__ = "2.7.0-RC1"  #
 
 import argparse
 import asyncio
